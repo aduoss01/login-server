62b86155e327a30aa299b98251104c0091cc0d70
asuh0114 <151424090+asuh0114@users.noreply.github.com>
2024-09-28 19:04:51 +0900

    update: register function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.cookies import SimpleCookie
 import json
+import os
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def end_headers(self):
@@ -15,12 +16,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write("".encode("utf-8"))
         
     def do_POST(self):
-        """
-        Method: POST
-        Path: /register
-        Body: {"id": "", "password": ""}
-        Result: {"success": true, "id": ""}
-        """
         if self.path == '/register':            
             content_length = int(self.headers['Content-Length'])
             body = self.rfile.read(content_length)
@@ -29,14 +24,36 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             id = payload['id']
             password = payload['password']
 
+            if os.path.exists('users.json'):
+                with open('users.json', 'r') as file:
+                    users = json.load(file)
+            else:
+                users = {}
+
+            if id in users:
+                self.send_response(400)
+                self.end_headers()
+                response = {"success": False, "message": "ID already registered"}
+                self.wfile.write(json.dumps(response, ensure_ascii=False).encode('utf-8'))
+            else:
+                users[id] = password
+
+                with open('users.json', 'w', encoding='utf-8') as file:
+                    json.dump(users, file)
+
+                self.send_response(200)
+                self.end_headers()
+                response = {"success": True, "id": id}
+                self.wfile.write(json.dumps(response, ensure_ascii=False).encode('utf-8'))
 
         """ 
-        Method: POST
-        Path: /login
-        Body: {"id": "", "password": ""}
-        Result: {"success": true, "id": "", "sessionId": ""}
-        Cookie: session=...
+            Method: POST
+            Path: /login
+            Body: {"id": "", "password": ""}
+            Result: {"success": true, "id": "", "sessionId": ""}
+            Cookie: session=...
         """
+
         if self.path == '/login':
             pass
         
